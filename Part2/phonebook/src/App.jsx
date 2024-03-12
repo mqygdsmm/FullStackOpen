fe38e342d699ac73f071f9720e1dfca4560bbed8
mqyg <mqygywl@outlook.com>
2024-03-12 11:27:14 +0800

    finished exercise2.6

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
