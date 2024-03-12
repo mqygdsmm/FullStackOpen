b776f1e6565beccc4333d8de6fadf7616d82fa03
mqyg <mqygywl@outlook.com>
2024-03-12 10:06:11 +0800

    finished exercise2.2 and 2.3

--- Part2/courseinfo/src/components/Course.jsx
+++ Part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,13 @@
 const Header = ({name}) => <h1>{name}</h1>
 const Content = ({parts}) => parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
+const Sum = ({parts}) => <p>Total of {parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} exercises</p>
 const Course = ({course}) => {
     let {id, name, parts} = course
     return (
         <div>
         <Header name={name}/>
         <Content parts={parts}/>
+        <Sum parts={parts} />
         </div>
     )
 }
