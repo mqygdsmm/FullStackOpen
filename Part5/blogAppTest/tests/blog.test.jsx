5f0650005e4900a523f2beeb0674170c03020640
mqyg <mqygywl@outlook.com>
2024-03-22 08:46:08 +0800

    finished exercise5.18

--- Part5/blogAppTest/tests/blog.test.jsx
+++ Part5/blogAppTest/tests/blog.test.jsx
@@ -5,14 +5,34 @@ describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('http://localhost:3003/api/testing/reset')
     await request.post('http://localhost:3003/api/user', {
-      username: 'yeweilun',
-      name: 'mqyg',
-      password: '546976125qq'
+      data: {
+        username: 'yeweilun',
+        name: 'mqyg',
+        password: '546976125'
+      }
     })
 
     await page.goto('http://localhost:5173')
   })
   test('display login form by default', async ({ page }) => {
     await expect(page.getByTestId('login')).toBeVisible()
+  })
+
+  describe('login', () => {
+    test('success login in', async ({ page }) => {
+      await page.getByTestId('username').fill('yeweilun')
+      await page.getByTestId('password').fill('546976125')
+      await page.getByRole('button', {name: 'Login'}).click()
+
+      await expect(page.getByText('yeweilun logged in')).toBeVisible()
+    })
+
+    test('invalid credential will fail', async ({ page }) => {
+      await page.getByTestId('username').fill('yeweilun')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', {name: 'Login'}).click()
+
+      await expect(page.getByText('invalid username or password')).toBeVisible()
+    }) 
   })
 })
--- Part5/bloglist-frontend/src/components/Login.jsx
+++ Part5/bloglist-frontend/src/components/Login.jsx
@@ -4,10 +4,10 @@ const Login = ({ username, password, onUsernameChange, onPasswordChange, handleL
       <h1>Log in  to application</h1>
       <form data-testId='login' onSubmit={handleLogin}>
         <div>
-            username <input type="text" value={username} onChange={onUsernameChange} />
+            username <input type="text" data-testId='username' value={username} onChange={onUsernameChange} />
         </div>
         <div>
-            password <input type="password" value={password} onChange={onPasswordChange} />
+            password <input type="password" data-testId='password' value={password} onChange={onPasswordChange} />
         </div>
         <button type="submit">Login</button>
       </form>
